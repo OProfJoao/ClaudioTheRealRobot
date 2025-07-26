b993840116d1099cb73f9e7b56f9fecbe0f3f20f
JoaoBornelli <joao.bornelli@edu.sc.senai.br>
2025-07-25 21:00:04 -0300

    Implement encoder handling for right and left sensors

--- src/main.py
+++ src/main.py
@@ -1,11 +1,36 @@
 from machine import Pin
 import time
 
-sensor = Pin(25, Pin.IN,Pin.PULL_UP)  # Ativa resistor interno
+
+rightCounter = 0
+leftCounter = 0
+leftLastDetection = time.ticks_ms()
+rightLastDetection = time.ticks_ms()
+
+
+rightEncoderPin = Pin(32, Pin.IN, Pin.PULL_UP)
+leftEncoderPin = Pin(33, Pin.IN, Pin.PULL_UP)
+
+
+def encoderHandler(pin):
+    global rightCounter, leftCounter
+    global leftLastDetection, rightLastDetection
+    current_ms = time.ticks_ms()
+
+    if pin is rightEncoderPin and current_ms > (rightLastDetection + 0):
+        rightCounter += 1
+        print(f'right: {rightCounter}')
+        rightLastDetection = current_ms
+    elif pin is leftEncoderPin and current_ms > (leftLastDetection + 0):
+        leftCounter += 1
+        leftLastDetection = current_ms
+        print(f'left: {leftCounter}')
+
+
+rightEncoderPin.irq(trigger=Pin.IRQ_FALLING, handler=encoderHandler)
+leftEncoderPin.irq(trigger=Pin.IRQ_FALLING, handler=encoderHandler)
 
 while True:
-    if sensor.value() == 0:  # Barreira interrompida (depende do sensor)
-        print("Objeto detectado!")
-    else:
-        print("Nada detectado")
-    time.sleep(0.2)
+
+    # print(f"Right: {rightCounter} / Left: {leftCounter}")
+    time.sleep(0.1)
